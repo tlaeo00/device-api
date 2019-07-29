cca2998ec21ec451be3c2b8bc4ee2a27ecdacb4a
심대진 <tlaeo00@poscoict.com>
2019-07-29 15:20:33 +0900

    log 정보 컨버팅

---
+++ src/main/java/com/example/deviceapi/common/CustomLog.java
@@ -0,0 +1,27 @@
+package com.example.deviceapi.common;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+/**
+ * @class : CustomLog.java
+ * @since : 2019-07-29.
+ * @author : 심대진, 고잉컴
+ * Description : 특정 로깅 컨버팅
+ * TODO : Pattern.matches, StringBuilder 사용해서 리팩토링
+ */
+public class CustomLog extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        String parsingMsg = "device_id"; // 파싱 문자열
+        String replaceMsg = "device_id = '...'"; // replace 문자열
+        
+        String msg = iLoggingEvent.getMessage();
+        msg = msg.replaceAll("(" + parsingMsg + " = '[a-zA-Z0-9]*')", replaceMsg);
+        msg = msg.replaceAll("(" + parsingMsg + " ='[a-zA-Z0-9]*')", replaceMsg);
+        msg = msg.replaceAll("(" + parsingMsg + "= '[a-zA-Z0-9]*')", replaceMsg);
+        msg = msg.replaceAll("(" + parsingMsg + "='[a-zA-Z0-9]*')", replaceMsg);
+        return msg;
+    }
+}
